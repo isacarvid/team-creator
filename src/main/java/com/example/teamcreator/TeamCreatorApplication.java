20c1e5bb15284d408e8fad0d507ec2302541365a
isacarvid <isacarv@kth.se>
2022-05-22 18:08:27 +0200

    first

---
+++ src/main/java/com/example/teamcreator/TeamCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamCreatorApplication.class, args);
+	}
+
+}
